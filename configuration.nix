c3137f95f8b1ffc781afb83a8ef31ad16b0aa2e7
Mychilli <Mychilli@gmail.com>
2025-05-07 16:32:54 -0500

    deleting some comments

--- configuration.nix
+++ configuration.nix
@@ -108,10 +108,8 @@
 
   # adding flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     tldr
     wget
     git
@@ -154,12 +152,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 
 }
--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    # NixOS official package source, here using the nixos-24.11 branch
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   home-manager = {
     url = "github:nix-community/home-manager/release-24.11";
@@ -11,9 +10,8 @@
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
-    # The host with the hostname `my-nixos` will use this configuration
     nixosConfigurations= {
-      mmalwick-nixos = nixpkgs.lib.nixosSystem{
+      mmalwick-nixos = nixpkgs.lib.nixosSystem{ #hostname
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
