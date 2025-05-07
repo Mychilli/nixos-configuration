647d19d2fe9df1befbaccd44e6cadfbbf1f89f36
Mychilli <Mychilli@gmail.com>
2025-05-07 16:57:04 -0500

    trying out some fish alias'

--- home.nix
+++ home.nix
@@ -9,6 +9,7 @@
     lazygit
     gh
     vscode
+    fish
   ];
   
   programs.git = {
@@ -16,12 +17,12 @@
     userName = "Mychilli";
     userEmail = "Mychilli@gmail.com";
   };
- # programs.fish = {
- #   enable = true;
- #   shellAliases = {
- #     ls = "ls -a";
- #   };
- # };
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "ls -a";
+    };
+  };
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
   
